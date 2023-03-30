e32ddf90986d877dc7862f1781fe9596e98cad0d
Arthur Henrique <70548506+Arthur2303@users.noreply.github.com>
2023-03-30 09:46:51 -0300

    Update desafio.sv

--- SPRINT2/desafio.sv
+++ SPRINT2/desafio.sv
@@ -1,41 +1,21 @@
 module desafio(input logic clk, // Vai receber o clock de 1Hz
-               output logic [6:0] cont);
+               output logic [0:6] out);
 
 
-    // Cátodo comum
-    // initial cont = 7'b1000000; // pino 0 do display ativo
+	 logic [0:6] HEX6;
+	 assign out = ~HEX6;
+	 
+    initial HEX6 = 7'b0000010; // pino 0 do display ativo
 
-    // always@(posedge clk)
-    //     begin 
-    //         if(cont > 7'b0000010) begin
-    //             cont <= cont >> 1;
-    //         end
-    //         else begin
-    //             cont <= 7'b1000000;
-    //         end
-    //     end
-
-    // Ânodo comum
-    initial cont = 7'b0111111;
-    
-        always@(posedge clk)
-        begin 
-            if(cont > 7'b1111101) begin
-                cont <= cont >> 1;
+    always@(posedge clk)
+       begin 
+            if(HEX6 != 7'b1000000) begin
+                HEX6 <= HEX6 <<  1;
             end
             else begin
-                cont <= 7'b0111111;
+                HEX6 <= 7'b0000010;
             end
         end
 
-endmodule
 
-/*
-Display
-0 -> ~7'b1000000;
-1 -> ~7'b0100000;
-2 -> ~7'b0010000;
-3 -> ~7'b0001000;
-4 -> ~7'b0000100;
-5 -> ~7'b0000010;
-*/
+endmodule
