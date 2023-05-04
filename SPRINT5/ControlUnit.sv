7f036c77faf8a984a992d116d61124e9cd2db1d1
Arthur Henrique <70548506+Arthur2303@users.noreply.github.com>
2023-05-04 09:31:16 -0300

    Update ControlUnit.sv

--- SPRINT5/ControlUnit.sv
+++ SPRINT5/ControlUnit.sv
@@ -1,13 +1,13 @@
 module ControlUnit(	input logic [5:0] OP, Funct,
-			output logic Jump, MemtoReg, MemWrite, Branch, ULASrc, RegDst, RegWrite,
-			output logic [2:0 ULAControl]);
+							output logic Jump, MemtoReg, MemWrite, Branch, ULASrc, RegDst, RegWrite,
+							output logic [2:0] ULAControl);
 
 
 	// Instrução do tipo R: OP = 6'b000000 e Funct variando
 	// ADDi OP = 6'b001000 e Funct 6'bxxxxxx
 
-	always_comb@(*)
-		begin 
+	always@(*)
+		begin 			
 			if(OP == 6'b000000 && Funct == 6'b100000) begin // ADD
 				{RegWrite,RegDst,ULASrc,ULAControl,Branch,MemWrite,MemtoReg,Jump} = 10'b1_1_0_010_0_0_0_0;
 			end
@@ -20,13 +20,20 @@ module ControlUnit(	input logic [5:0] OP, Funct,
 			if(OP == 6'b000000 && Funct == 6'b100101) begin // OR
 				{RegWrite,RegDst,ULASrc,ULAControl,Branch,MemWrite,MemtoReg,Jump} = 10'b1_1_0_001_0_0_0_0;
 			end
+			if(OP == 6'b000000 && Funct == 6'b100111) begin // NOR
+				{RegWrite,RegDst,ULASrc,ULAControl,Branch,MemWrite,MemtoReg,Jump} = 10'b1_1_0_011_0_0_0_0;
+			end
 			if(OP == 6'b000000 && Funct == 6'b101010) begin // SLT
 				{RegWrite,RegDst,ULASrc,ULAControl,Branch,MemWrite,MemtoReg,Jump} = 10'b1_1_0_111_0_0_0_0;
 			end
 			if(OP == 6'b001000) begin // ADDi
-				{RegWrite,RegDst,ULASrc,ULAControl,Branch,MemWrite,MemtoReg,Jump} = 10'b1_1_0_010_0_0_0_0;
+				{RegWrite,RegDst,ULASrc,ULAControl,Branch,MemWrite,MemtoReg,Jump} = 10'b1_0_1_010_0_0_0_0;
 			end
 		end
 
 
 endmodule
+
+/*
+casex({OP,Funct})
+12'b000000_100000: {RegWrite,RegDst,ULASrc,ULAControl,Branch,MemWrite,MemtoReg,Jump} = 10'b1_1_0_010_0_0_0_0;*/
