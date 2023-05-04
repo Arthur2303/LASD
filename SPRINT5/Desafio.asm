805b1d54e634ac49dacd0a71a59edb06ea840b6a
Arthur Henrique <70548506+Arthur2303@users.noreply.github.com>
2023-05-04 09:30:31 -0300

    Update Desafio.asm

--- SPRINT5/Desafio.asm
+++ SPRINT5/Desafio.asm
@@ -1,7 +1,7 @@
 ; Desloca o valor do registrador $s1 4 bits para a esquerda
 
 ADDI $s1, $zero, 202 ; 11001010 ou  0xCA
-ADD $s1, $s1, $s1
+ADD $s2, $s1, $s1
 ADD $s3, $s2, $s2
 ADD $s4, $s3, $s3
 ADD $s5, $s4, $s4 ; 1010000 ou  0xA0
