4afa69dcec7a11602deb7f45ab8c22d5fe501677
Arthur Henrique <70548506+Arthur2303@users.noreply.github.com>
2023-05-11 08:57:41 -0300

    Update Mod_Teste.sv

--- SPRINT6/Mod_Teste.sv
+++ SPRINT6/Mod_Teste.sv
@@ -34,10 +34,10 @@ LCD_TEST MyLCD (
 );
 //---------- modifique a partir daqui --------
 
-	//	SPRINT6
+	//SPRINT6
 	logic w_RegDst, w_ALUSrc, w_RegWrite, w_Jump, w_MemtoReg, w_MemWrite,w_Branch, clk;
 	logic [2:0] w_wa3, w_ULAControl;
-	logic [7:0] w_rd1SrcA, w_rd2, w_SrcB, w_ULAResultWd3, w_PCp1, w_PC, w_RData w_wd3;
+	logic [7:0] w_rd1SrcA, w_rd2, w_SrcB, w_ULAResultWd3, w_PCp1, w_PC, w_RData, w_wd3;
 	logic [31:0] w_RD; 
 	
 	assign LEDG[1] = ~clk;	// CLK
@@ -47,23 +47,23 @@ LCD_TEST MyLCD (
 	assign w_d0x4 = w_PC;
 	
 	
-	FreqDivisor #(.BordaDeSubida(25000000)) divisorD(.CLOCK_50( CLOCK_50 ), .LEDG( clk ));
+	FreqDivisor #(.BordaDeSubida(25000000)) divisorD (.CLOCK_50( CLOCK_50 ), .LEDG( clk ));
 	
 	PC ProgramCounter( .PCin( w_PCp1 ), .clk( clk ), .rst( KEY[1] ), .PC( w_PC ));
 	
 	Adder1 add(	.In( w_PC ), .Out( w_PCp1 )); 
 	
-	RomInstMem ROM( .address( w_ULAResultWd3 ), .data( w_rd2 ), .clock( CLOCK_50 ), .wren( w_MemWrite ), .q( w_RData ));
+	RomInstMem ROM( .address( w_PC ), .clock( CLOCK_50 ), .q( w_RD ));
 	
 	Mux2x1 #(.N(8)) MuxDDest( .in0( w_ULAResultWd3 ), .in1( w_RData ), .Sel( w_MemtoReg ), .out( w_wd3 ));
 	
-	RamDataMem RAM( .address( w_PC ), .clock( CLOCK_50 ), .q(  ));
+	RamDataMem RAM( .address( w_ULAResultWd3 ), .data( w_rd2 ), .clock( CLOCK_50 ), .wren( w_MemWrite ), .q( w_RData ));
 	
 	ControlUnit control(	.OP( w_RD[31:26] ), .Funct( w_RD[5:0] ), .RegDst( w_RegDst ), .RegWrite( w_RegWrite ), .ULAControl( w_ULAControl ), .ULASrc( w_ALUSrc ),
 								.Jump( w_Jump ), .MemtoReg( w_MemtoReg ), .MemWrite( w_MemWrite ), .Branch( w_Branch ));
 
 	
-	RegisterFile #(.N(8)) register(	.wd3(   w_ULAResultWd3   ), .wa3(  w_wa3  ), .ra1( w_RD[25:21] ), .ra2( w_RD[20:16] ), 
+	RegisterFile #(.N(8)) register(	.wd3(   w_wd3   ), .wa3(  w_wa3  ), .ra1( w_RD[25:21] ), .ra2( w_RD[20:16] ), 
 												.we3( w_RegWrite  ), .clk(   clk  ), .rst(   KEY[1]  ), .rd1(  w_rd1SrcA  ), 
 												.rd2(    w_rd2    ), .S0(	w_d0x0	), .S1(	w_d0x1	), .S2(	w_d0x2	), .S3(	w_d0x3	), 
 												.S4(	w_d1x0	),.S5(	w_d1x1	), .S6(	w_d1x2	), .S7(	w_d1x3	));
@@ -76,7 +76,6 @@ LCD_TEST MyLCD (
 	
 	
 	Mux2x1 #(.N(4)) MuxWR(	.in0( w_RD[20:16] ), .in1( w_RD[15:11]), .Sel( w_RegDst ), .out( w_wa3 ));
-
 	
 
 	
