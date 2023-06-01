bb2b39a0eafc79bd5cafc4fab16dd21f10b42c64
Arthur Henrique <70548506+Arthur2303@users.noreply.github.com>
2023-06-01 09:21:32 -0300

    Update SPRINT8.asm

--- SPRINT8/SPRINT8.asm
+++ SPRINT8/SPRINT8.asm
@@ -1,14 +1,14 @@
-# Código assembly necessário para a SPRINT8. OBS: Feito no MARS 4.5
+# CÃ³digo assembly necessÃ¡rio para a SPRINT8. OBS: Feito no MARS 4.5
 
-addi $s1, $zero, 9  # Número de entrada
-addi $s2, $zero, 1  # Número que será comparado
-addi $s4, $zero, 4  # Quantidade de iterações / Número máximo após os deslocamentos
+addi $s1, $zero, 9  # NÃºmero de entrada
+addi $s2, $zero, 1  # NÃºmero que serÃ¡ comparado
+addi $s4, $zero, 4  # Quantidade de iteraÃ§Ãµes / NÃºmero mÃ¡ximo apÃ³s os deslocamentos
 add $s5, $zero, $zero # Contador de 1
 add $s6, $zero, 1 
 
 loop: 	and $s3, $s1, $s2
 	beq $s3,$zero, label
-	addi $s5, $s5, $s1
+	add $s5, $s5, $s1
 label:	add $s2, $s2, $s2
 	sub $s4, $s4, $s6
 	beq $zero, $s4, loop
